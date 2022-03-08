e2175e6b652fe700c767228f6bcaa4f4a537ef78
raghad <RAGHAD.MOHAMMED.ruh.k@tuwaiq.edu.sa>
2022-03-08 14:47:29 +0300

    Initial commit

---
+++ codingChallenge.kt
@@ -0,0 +1,30 @@
+fun main(){
+
+}
+
+#1st Challenge
+fun receivesIntegers(intArray : Array)  : Int{
+ for (i in intArray) {
+
+ if (i != 1){
+ newArray.add = i
+ println(i)} }
+if
+
+
+}
+
+#4th Challenge
+fun receiveString(string : String) : Boolean{
+
+if (string == "()" or "({ })" or "{{()}}" or "[{}]" or "{[(}])" ){
+
+return true
+
+} else
+
+if (string == "}{" or ")({}" or "({ }})" or "{{()}" or "{[}])" ){
+
+return false
+
+}}
